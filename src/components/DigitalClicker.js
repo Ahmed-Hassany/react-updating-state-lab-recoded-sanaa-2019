8b59feae0ec1c1b1a18fba824bbf950e5f69ba94
ahmed-hassany <ahmedalhassany1@gmail.com>
2019-12-15 11:27:26 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+export default class DigitalClicker extends Component{
+    constructor(){
+        super();
+        this.state={
+            timesClicked :0
+        };
+    }
+    increaseTimesClicked = ()=>{
+        this.setState(prevState=>({
+            timesClicked: prevState.timesClicked + 1
+        })
+    )
+    }
+    render(){
+        return(
+            <button onClick={this.increaseTimesClicked}>{this.state.timesClicked}</button>
+        );
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,48 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+
+    constructor(){
+        super()
+        this.state ={
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+    bitrateBtnHandler=()=>{
+        this.setState({
+            settings: {
+              ...this.state.settings,
+              bitrate: 12
+            }
+          })
+    }
+
+    resolutionBtnHandler=()=>{
+        this.setState({
+            settings: {
+              ...this.state.settings,
+              video: {
+                resolution: '720p'
+              }
+            }
+          })
+    }
+
+    render(){
+        return(
+                <div>
+                    <button onClick={this.bitrateBtnHandler} className="bitrate">bitrate</button>
+                    <button onClick={this.resolutionBtnHandler} className="resolution">resolution</button>
+                </div>
+        )
+    }
+
+}
